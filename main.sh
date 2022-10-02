6ab91eca7a9aed298304428cc3276504fd067c83
Johan de Vries <johandevries@c2-academy.com>
2022-10-02 19:09:14 +0000

    Add comments to /etc/passwd

--- main.sh
+++ main.sh
@@ -13,19 +13,19 @@ main()
 
    # Setup users
 	jasperj_pass="strato_NL"
-	useradd jasperj --create-home --gid 1050 --shell /bin/bash 2>/dev/null 
+	useradd jasperj --create-home --gid 1050 --shell /bin/bash --comment "Jasper Jolles,Web Design,+31 71 223 3777,,jasperj@ql-robotics.com" 2>/dev/null 
 	echo "jasperj:$jasperj_pass" | chpasswd
 
    borish_pass="icarus123!@!" 
    useradd borish --create-home --gid 1051 --shell /bin/sh 2>/dev/null
 	echo "borish:$borish_pass" | chpasswd
-   useradd laurensm --create-home --gid 1051 --shell /bin/sh 2>/dev/null
-   useradd dannym --create-home --gid 1051 --shell /bin/sh 2>/dev/null
-   useradd antong --create-home --gid 1051 --shell /bin/sh 2>/dev/null
-   useradd nicolass --create-home --gid 1051 --shell /bin/sh 2>/dev/null
-   useradd maryl --create-home --gid 1052 --shell /bin/sh 2>/dev/null
-   useradd lisag --create-home --gid 1050 --shell /bin/bash 2>/dev/null
-   useradd sergoz  --create-home --gid 1050 --shell /bin/bash 2>/dev/null
+   useradd laurensm --create-home --gid 1051 --shell /bin/sh --comment "Laurens Martha,Engineering,+49 30 175944864,,laurensm@ql-robotics.com" 2>/dev/null
+   useradd dannym --create-home --gid 1051 --shell /bin/sh --comment "Danny Moskov,Engineering,+49 30 093197904,,dannym@ql-robotics.com" 2>/dev/null
+   useradd antong --create-home --gid 1051 --shell /bin/sh --comment "Anton Goertov,Engineering,+49 30 638718869,,antong@ql-robotics.com" 2>/dev/null
+   useradd nicolass --create-home --gid 1051 --shell /bin/sh --comment "Anton Goertov,Engineering,+49 30 638718869,,nicolass@ql-robotics.com" 2>/dev/null
+   useradd maryl --create-home --gid 1052 --shell /bin/sh --comment "Mary Lim-Lee,Legal,+49 30 050979754,,maryl@ql-robotics.com" 2>/dev/null
+   useradd lisag --create-home --gid 1050 --shell /bin/bash  --comment "Lisa Grande,Web Design,+49 30 759635389,,lisag@ql-robotics.com" 2>/dev/null
+   useradd sergoz  --create-home --gid 1050 --shell /bin/bash --comment "Sergo Zanson,SysAdmin,+49 17 717817048,,antong@ql-robotics.com" 2>/dev/null
    usermod -a -G legal laurensm 2>/dev/null
    usermod -a -G sudo sergoz 2>/dev/null
    
