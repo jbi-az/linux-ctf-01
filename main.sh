07c4417ca46f369069dc30b6fb09410572b00225
Johan de Vries <johandevries@c2-academy.com>
2022-10-02 18:37:18 +0000

    Change index folder

--- main.sh
+++ main.sh
@@ -13,19 +13,28 @@ main()
 
    # Setup users
 	jasperj_pass="strato_NL"
-	useradd jasperj --home-dir /usr/share/nginx --gid 1050 --shell /bin/bash 2>/dev/null 
+	useradd jasperj --create-home --gid 1050 --shell /bin/bash 2>/dev/null 
 	echo "jasperj:$jasperj_pass" | chpasswd
 
    borish_pass="icarus123!@!" 
    useradd borish --create-home --gid 1051 --shell /bin/sh 2>/dev/null
 	echo "borish:$borish_pass" | chpasswd
-
+   useradd laurensm --create-home --gid 1051 --shell /bin/sh 2>/dev/null
+   useradd dannym --create-home --gid 1051 --shell /bin/sh 2>/dev/null
+   useradd antong --create-home --gid 1051 --shell /bin/sh 2>/dev/null
+   useradd nicolass --create-home --gid 1051 --shell /bin/sh 2>/dev/null
+   useradd maryl --create-home --gid 1052 --shell /bin/sh 2>/dev/null
+   useradd lisag --create-home --gid 1050 --shell /bin/bash 2>/dev/null
+   useradd sergoz  --create-home --gid 1050 --shell /bin/bash 2>/dev/null
+   usermod -a -G legal laurensm 2>/dev/null
+   usermod -a -G sudo sergoz 2>/dev/null
+   
    # Application
    apt update && apt install -y zip figlet nginx 
  
    # ---> Target 1 <---
    # Mission 1-3
-   mv html/ /usr/share/nginx/ 
-
+   mv html/* /var/www/html/ 
+   
 }
 main
