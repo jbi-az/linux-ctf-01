beaf53e28cb6215be878f0f12962a14e1d02b5de
Johan de Vries <johandevries@c2-academy.com>
2022-10-02 17:49:10 +0000

    Add groups and users

--- main.sh
+++ main.sh
@@ -6,16 +6,26 @@ set -e
 
 main()
 {  
+   # Setup groups
+   groupadd webdesign --gid 1050
+   groupadd engineering --gid 1051
+   groupadd legal --gid 1052
+
    # Setup users
-	jasperj_pass="cyber123!"
-	useradd jasperj -m -s /bin/bash 2>/dev/null 
+	jasperj_pass="strato_NL"
+	useradd jasperj --home-dir /usr/share/nginx --gid 1050 --shell /bin/bash 2>/dev/null 
 	echo "jasperj:$jasperj_pass" | chpasswd
+
+   borish_pass="icarus123!@!" 
+   useradd borish --create-home --shell --gid 1051 --shell /bin/sh 2>/dev/null
+	echo "borish:$borish_pass" | chpasswd
 
    # Application
    apt update && apt install -y zip figlet nginx 
  
    # ---> Target 1 <---
    # Mission 1-3
+   mv html/ /usr/share/nginx/ 
 
 }
 main
